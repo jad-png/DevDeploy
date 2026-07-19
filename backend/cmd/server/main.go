c1e3baa1b3c556e938a323014902826f6f2dc567
jad-png <j.belassiria3993@uca.ac.ma>
2026-07-19 22:42:24 +0100

    chore(main): bootstrap logger and PostgreSQL connection

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"context"
+	"devdeply/internal/adapters/postgres"
 	"devdeply/internal/config"
 	"devdeply/internal/logger"
+	"fmt"
 	"log"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	logg := logger.New(cfg.App)
+	fmt.Printf("%+v\n", cfg)
+	logger := logger.New(cfg.App)
 
-	logg.Error("test app")
+	db, err := postgres.New(ctx, cfg.Database, logger)
+	if err != nil {
+		logger.Error("failed to connect to postgres", "error", err)
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	logger.Info("application started successfully")
 }
